fd31c7ad350eaa210f5b5f3fc69f977e2a04d6f7
xuwentao <wewentao@wepie.com>
2021-01-29 15:15:27 +0800

    uts namespace

---
+++ chapter1/main.go
@@ -0,0 +1,22 @@
+package chapter1
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main(){
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags:syscall.ClONE_NEWUTS,
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil{
+		log.Fatalln(err)
+	}
+}
