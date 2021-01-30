5cd91a5e8243615442879e507ed0a332fce1ff7b
xuwentao <yellowbluewhite@foxmail.com>
2021-01-29 21:37:30 -0500

    namespace

--- chapter1/main.go
+++ chapter1/main.go
@@ -7,16 +7,29 @@ import (
 	"syscall"
 )
 
-func main(){
+func main() {
 	cmd := exec.Command("sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:syscall.ClONE_NEWUTS,
-	}
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER| syscall.CLONE_NEWNET,
+		UidMappings: []syscall.SysProcIDMap{
+			{	ContainerID: 0,
+			HostID:      0,
+			Size:        1,
+		},
+		},
+		GidMappings: []syscall.SysProcIDMap{
 
+			{ContainerID: 0,
+				HostID: 0,
+				Size:   1,
+			},
+		},
+	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil{
+	if err := cmd.Run(); err != nil {
 		log.Fatalln(err)
 	}
 }
