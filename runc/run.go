0c028601d3484a06694f1d1686de2ee705aec6c4
xuwentao <wewentao@wepie.com>
2021-01-31 10:48:09 +0800

    add cgroup

--- runc/run.go
+++ runc/run.go
@@ -1,51 +1,37 @@
 package main
 
 import (
-	"fmt"
-	log "github.com/sirupsen/logrus"
-	"github.com/urfave/cli"
+	"github.com/huhuhudia/docker/runc/cgroups"
+	"github.com/huhuhudia/docker/runc/cgroups/subsystems"
 	"github.com/huhuhudia/docker/runc/container"
+	log "github.com/sirupsen/logrus"
 	"os"
+	"strings"
 )
 
-var runCommand = cli.Command{
-	Name: "run",
-	Usage: `Create a container with namespace and cgroups limit
-			mydocker run -ti [command]`,
-	Flags: []cli.Flag{
-		cli.BoolFlag{
-			Name:  "ti",
-			Usage: "enable tty",
-		},
-	},
-	Action: func(context *cli.Context) error {
-		if len(context.Args()) < 1 {
-			return fmt.Errorf("Missing container command")
-		}
-		cmd := context.Args().Get(0)
-		tty := context.Bool("ti")
-		Run(tty, cmd)
-		return nil
-	},
-}
 
-var initCommand = cli.Command{
-	Name:  "init",
-	Usage: "Init container process run user's process in container. Do not call it outside",
-	Action: func(context *cli.Context) error {
-		log.Infof("init come on")
-		cmd := context.Args().Get(0)
-		log.Infof("command %s", cmd)
-		err := container.RunContainerInitProcess(cmd, nil)
-		return err
-	},
-}
-
-func Run(tty bool, command string) {
-	parent := container.NewParentProcess(tty, command)
+func Run(tty bool, comArray []string, res *subsystems.ResourceConfig) {
+	parent, writePipe := container.NewParentProcess(tty)
+	if parent == nil {
+		log.Errorf("New parent process error")
+		return
+	}
 	if err := parent.Start(); err != nil {
 		log.Error(err)
 	}
+	// use mydocker-cgroup as cgroup name
+	cgroupManager := cgroups.NewCgroupManager("mydocker-cgroup")
+	defer cgroupManager.Destroy()
+	cgroupManager.Set(res)
+	cgroupManager.Apply(parent.Process.Pid)
+
+	sendInitCommand(comArray, writePipe)
 	parent.Wait()
-	os.Exit(-1)
+}
+
+func sendInitCommand(comArray []string, writePipe *os.File) {
+	command := strings.Join(comArray, " ")
+	log.Infof("command all is %s", command)
+	writePipe.WriteString(command)
+	writePipe.Close()
 }
